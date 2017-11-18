3505cd4ad6d749c273ddb66e2bf3c371f53c146e
Abel Pinheiro de Figueiredo <abelpinheiro95@gmail.com>
2017-11-18 02:32:24 -0300

    Add player object

---
+++ SpaceInvaders/Assets/Scripts/movPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movPlayer : MonoBehaviour {
+    public float minBound, maxBound;
+    public float speed;
+    private Transform player;
+
+    void Start () {
+        player = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float posX = Input.GetAxis("Horizontal");
+        if (player.position.x < minBound && posX < 0)
+        {
+            posX = 0;
+        }
+        else if (player.position.x > maxBound && posX > 0)
+        {
+            posX = 0;
+        }
+
+        player.position += Vector3.right * posX * speed;
+	}
+}
